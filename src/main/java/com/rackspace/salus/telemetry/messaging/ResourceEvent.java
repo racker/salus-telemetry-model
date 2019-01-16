382a2de12962f24e1343f50dc3b9b70643c32343
James Buchan <james.buchan@rackspace.com>
2019-01-16 11:02:53 -0700

    Add resource events

---
+++ src/main/java/com/rackspace/salus/telemetry/messaging/ResourceEvent.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 Rackspace US, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rackspace.salus.telemetry.messaging;
+
+import java.util.Map;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.rackspace.salus.common.messaging.KafkaMessageKey;
+import com.rackspace.salus.telemetry.model.Resource;
+import lombok.Data;
+
+@KafkaMessageKey(properties = {"tenantId", "operation"})
+@Data
+public class ResourceEvent {
+    @NotBlank
+    Resource resource;
+
+    @NotNull
+    Map<String,String> oldLabels;
+
+    @NotBlank
+    String operation;
+
+    @NotNull
+    boolean presenceMonitorChange;
+
+    @NotBlank
+    String tenantId;
+}
