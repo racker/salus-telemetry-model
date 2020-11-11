e22c78c8651bae634e2775c052d4c96895ba1769
James Buchan <james.buchan@rackspace.com>
2020-11-11 15:40:55 -0700

    Add agent connection history pruning

---
+++ src/main/resources/db/migration/mysql/V15_0__prune_old_agent_connections.sql
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2020 Rackspace US, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+CREATE EVENT IF NOT EXISTS cleaning ON SCHEDULE EVERY 1 DAY ENABLE
+    DO
+    DELETE FROM agent_history
+    WHERE connected_timestamp < CURRENT_TIMESTAMP - INTERVAL 1 MONTH
+      AND disconnected_timestamp IS NOT NULL;
