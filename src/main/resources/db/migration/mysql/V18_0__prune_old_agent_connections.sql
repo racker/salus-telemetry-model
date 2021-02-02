ef3435370429a20763a84b1b4d5018d30ffc7e55
Nick Sunday <41025995+nicksunday@users.noreply.github.com>
2021-02-02 16:14:23 -0600

    Update V18_0__prune_old_agent_connections.sql

--- src/main/resources/db/migration/mysql/V18_0__prune_old_agent_connections.sql
+++ src/main/resources/db/migration/mysql/V18_0__prune_old_agent_connections.sql
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 CREATE EVENT IF NOT EXISTS agent_history_cleaning ON SCHEDULE EVERY 1 DAY ENABLE
